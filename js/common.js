fa352415649362f8a69dc042398c3bff77d06681
Unknown <emrdl7@gmail.com>
2017-10-16 11:16:20 +0900

    예약단까지 코딩 완료

--- js/common.js
+++ js/common.js
@@ -1,40 +1,93 @@
 
 $(document).ready(function () {
 
+
+
+
+
   $(window).resize(function(){
-    $("header *").removeAttr("style");
     $(".no-resize").removeAttr("style");
+    $(".no-resize .outline").removeAttr("style");
+
+    $(".toggle button").each(function() {
+      if ($(this).hasClass("active")) {
+          $(this).click();
+      }
+    });
+
   });
 
 
   $(".toggle .search").click(function(){
-    $(this).toggleClass("active");
-    $(this).find("i").toggleClass("icon-close").toggleClass("icon-search");
-    $("#header .search-form").toggle();
 
-    $("body").toggleClass("modal");
+      if ($(this).hasClass("active")) {
 
-    if ($(".toggle .menu").hasClass("active")) {
-      $(".toggle .menu").removeClass("active");
-      $(".toggle .menu").find("i").toggleClass("icon-close").toggleClass("icon-menu");
-      $("#header .buttons").toggle();
-    }
+        $("body").removeClass("modal");
+        $(this).removeClass("active");
+        $(this).find("i").removeClass("icon-close").addClass("icon-search");
+        $("#header .search-form").hide();
+
+      } else {
+
+        $("body").addClass("modal");
+        $(this).addClass("active");
+        $(this).find("i").addClass("icon-close").removeClass("icon-search");
+        $("#header .search-form").show();
+
+      }
+
+      if ($(".toggle .menu").hasClass("active")) {
+        $("#header .buttons").hide();
+        $(".toggle .menu").removeClass("active");
+        $(".toggle .menu").find("i").removeClass("icon-close").addClass("icon-menu");
+      }
   });
 
 
   $(".toggle .menu").click(function(){
-    $(this).toggleClass("active");
-    $(this).find("i").toggleClass("icon-close").toggleClass("icon-menu");
-    $("#header .buttons").toggle();
 
-    $("body").toggleClass("modal");
+      if ($(this).hasClass("active")) {
 
-    if ($(".toggle .search").hasClass("active")) {
-      $(".toggle .search").removeClass("active");
-      $(".toggle .search").find("i").toggleClass("icon-close").toggleClass("icon-search");
-      $("#header .search-form").toggle();
-    }
+        $("body").removeClass("modal");
+        $(this).removeClass("active");
+        $(this).find("i").removeClass("icon-close").addClass("icon-menu");
+        $("#header .buttons").hide();
+
+      } else {
+
+        $("body").addClass("modal");
+        $(this).addClass("active");
+        $(this).find("i").addClass("icon-close").removeClass("icon-menu");
+        $("#header .buttons").show();
+
+      }
+
+      if ($(".toggle .search").hasClass("active")) {
+        $("#header .search-form").hide();
+        $(".toggle .search").removeClass("active");
+
+        $(".toggle .search").find("i").removeClass("icon-close").addClass("icon-search");
+
+      }
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   $("div.count .icon-plus-circle").click(function(){
   		$(this).parent().find("input").val() + 1;
@@ -81,6 +134,54 @@ $(document).ready(function () {
       }
     });
 
+
+    $("button.lodge-type").click(function(){
+      $(this).toggleClass("active");
+      $("div.lodge-type").toggle();
+
+      var offset = $(this).offset();
+      $("div.lodge-type").css("height", $(document).height());
+      if ($(window).width() < 1199) {
+        $('html, body').animate({scrollTop: $(this).offset().top - 200}, 500);
+      } else {
+        $("div.lodge-type .outline").css({top: offset.top, left: offset.left });
+      }
+    });
+
+    $("button.lodge-price").click(function(){
+      $(this).toggleClass("active");
+      $("div.lodge-price").toggle();
+
+      var offset = $(this).offset();
+      $("div.lodge-price").css("height", $(document).height());
+      if ($(window).width() < 1199) {
+        $('html, body').animate({scrollTop: $(this).offset().top - 200}, 500);
+      } else {
+        $("div.lodge-price .outline").css({top: offset.top, left: offset.left });
+      }
+    });
+
+    $("div.lodge-type button").click(function(){
+      $("div.lodge-type").toggle();
+      $("button.lodge-type").toggleClass("active");
+    });
+
+    $("div.lodge-price button").click(function(){
+      $("div.lodge-price").toggle();
+      $("button.lodge-price").toggleClass("active");
+    });
+
+
+    $(".btn-map").click(function(){
+      $(this).toggleClass("active");
+      $(".item-map").toggle();
+    });
+
+
+
+
+
+
     $("div.count button").click(function(){
       $("button.count").toggleClass("active");
       $("div.count").toggle();
@@ -91,22 +192,103 @@ $(document).ready(function () {
 
     });
 
+  $("fieldset.lodge-search .close").click(function(){
+      $("fieldset.lodge-search").toggle();
+      $("div.region").hide();
+  });
+
+  $("article#navigator .modify").click(function(){
+      $("fieldset.lodge-search").toggle();
+
+  });
 
 
-  $('.input-date').dateRangePicker({
-    separator: ' ~ ',
-    language:'ko',
-    getValue: function()
-  	{
-  		return $(this).find("strong").html();
-  	},
-  	setValue: function(s)
-  	{
-  		$(this).find("strong").html(s);
+
+
+
+  $(".agree dt button").click(function(){
+    $(this).parent().toggleClass("open");
+    $(this).parent().next().toggleClass("open");
+  });
+
+
+  $(".agree #all").click(function(){
+    var checkBoxes = $(".agree dt input");
+    checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+  });
+
+  $(".item-list p.image").each(function() {
+    $(this).css("background", "url(" + $(this).find("img").attr("src") + ") no-repeat center / cover");
+  });
+
+  $(".big-photo .slider li").each(function() {
+    $(this).css("background", "url(" + $(this).find("img").attr("src") + ") no-repeat center / cover");
+  });
+
+  $(".room-info .photo").each(function() {
+    $(this).css("background", "url(" + $(this).find("img").attr("src") + ") no-repeat center / cover");
+  });
+
+
+  /* STAR_RATE*/
+  var $starRate = $('.opinion .grade');
+
+  if ($starRate.length > 0) $starRate.on('click', 'a', function() {
+    var starValue = $(this).attr("data");
+    $starRate.find('a').each(function() {
+      $(this).find("i").removeClass("on");
+
+      if ($(this).attr("data") <= starValue) {
+        $(this).find("i").addClass("on");
+      }
+    });
+  });
+
+
+
+  if ($("#content.main section").hasClass("hero") || $("#content").hasClass("lodge")) {
+
+    $('.input-date').dateRangePicker({
+      separator: ' ~ ',
+      language:'ko',
+      getValue: function()
+    	{
+    		return $(this).find("strong").html();
+    	},
+    	setValue: function(s)
+    	{
+    		$(this).find("strong").html(s);
+      }
+
+    });
+
+  }
+
+
+    if ($("#content section").hasClass("view-info")) {
+      var bigSlider = $('.big-photo .slider').bxSlider({
+        pager: false,
+        controls: false,
+        infiniteLoop: true
+      });
+
+      $(".slider-prev").click(function(){
+        bigSlider.goToPrevSlide();
+      });
+
+      $(".slider-next").click(function(){
+        bigSlider.goToNextSlide();
+      });
+
     }
 
 
-  });
+
+
+
+
+
+
 
 
 });
